0ee5f5a538d31b5b83c63968304e6f5ca3bd6a65
kadarbalint <65283137+kadarbalint@users.noreply.github.com>
2021-03-01 08:36:54 +0100

    Add files via upload

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <p>{{task.question}}</p>
+    <div v-katex="task.katex"></div>
+    <div v-if="task.img">
+     <img v-bind:src="task.img"> 
+    </div>
+    <div v-for=" answear in task.answears" :key=answear>
+      <input type="radio" :id="answear" :value="answear" v-model="picked">
+      <label> <span v-katex="answear"> </span> </label>
+      <br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  props: ['task'],
+  data: () => ({
+    picked: "nincs",
+  }),
+}
+  
+</script>
+
+<style scoped>
+.container{
+  background-color:whitesmoke;
+  border-radius: 10px;
+}
+</style>
